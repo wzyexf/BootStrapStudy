020df8174188d7e29e43dd1efe6d9d8cd7cd5d0e
wzyexf <wzyexf@126.com>
2017-01-06 14:58:47 +0800

    初始化

---
+++ BootStrapStudy/BootStrapStudy/Controllers/StudyController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootStrapStudy.Controllers
+{
+    public class StudyController : Controller
+    {
+         public ActionResult Index()
+        {
+            int a = 1;
+
+            ViewBag.a = a;
+            return View();
+        }
+
+        public ActionResult example()
+        {
+            return View();
+
+        }
+        public ActionResult example2()
+        {
+            return View();
+
+        }
+        public ActionResult example3()
+        {
+            return View();
+
+        }
+
+    }
+}
---
+++ BootStrapStudy/BootStrapStudy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootStrapStudy.MvcApplication" Language="C#" %>
